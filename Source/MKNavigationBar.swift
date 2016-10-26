4549a3011617593bde17be5b9113b0be003f6fa5
iTeam Technology <info@iteamtech.net>
2016-10-26 13:07:42 +0530

    Add files via upload
    Compatible with swift3

--- Source/MKNavigationBar.swift
+++ Source/MKNavigationBar.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 @IBDesignable
-public class MKNavigationBar: UINavigationBar {
+open class MKNavigationBar: UINavigationBar {
 
-    let statusView = UIView(frame: CGRectMake(0, -20, UIScreen.mainScreen().bounds.size.width, 20))
+    let statusView = UIView(frame: CGRect(x: 0, y: -20, width: UIScreen.main.bounds.size.width, height: 20))
     
-    @IBInspectable public var elevation: CGFloat = 0 {
+    @IBInspectable open var elevation: CGFloat = 0 {
         didSet {
             drawShadow()
         }
     }
-    @IBInspectable public var shadowOpacity: Float = 0.5 {
+    @IBInspectable open var shadowOpacity: Float = 0.5 {
         didSet {
             drawShadow()
         }
     }
     
-    @IBInspectable public var color: UIColor = UIColor.whiteColor() {
+    /*@IBInspectable public var color: UIColor = UIColor.whiteColor() {
         didSet {
             UINavigationBar.appearance().barTintColor = self.color
         }
@@ -41,7 +41,7 @@ public class MKNavigationBar: UINavigationBar {
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: self.tintColor]
             UINavigationBar.appearance().tintColor = self.tintColor
         }
-    }
+    }*/
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -53,29 +53,29 @@ public class MKNavigationBar: UINavigationBar {
         setup()
     }
     
-    override public func layoutSubviews() {
+    override open func layoutSubviews() {
         drawShadow()
         super.layoutSubviews()
     }
     
-    private func setup() {
-        self.statusView.backgroundColor = self.darkColor
+    fileprivate func setup() {
+        //self.statusView.backgroundColor = self.darkColor
         self.addSubview(self.statusView)
-        UINavigationBar.appearance().barTintColor = self.color
-        UINavigationBar.appearance().backgroundColor = self.tintColor
+        //UINavigationBar.appearance().barTintColor = self.color
+       // UINavigationBar.appearance().backgroundColor = self.tintColor
         UINavigationBar.appearance().tintColor = self.tintColor
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: self.tintColor]
     }
     
-    private func drawShadow() {
+    fileprivate func drawShadow() {
         if elevation > 0 {
             let shadowPath = UIBezierPath(rect: bounds)
             layer.masksToBounds = false
             layer.shadowRadius = elevation
-            layer.shadowColor = UIColor.blackColor().CGColor
+            layer.shadowColor = UIColor.black.cgColor
             layer.shadowOffset = CGSize(width: 1, height: 1);
             layer.shadowOpacity = shadowOpacity
-            layer.shadowPath = shadowPath.CGPath
+            layer.shadowPath = shadowPath.cgPath
         }
     }
     
--- Source/MKSegmentControl.swift
+++ Source/MKSegmentControl.swift
@@ -1,14 +1,28 @@
 //
-//  MKLabel.swift
+//  MKTextField.swift
 //  MaterialKit
 //
-//  Created by Le Van Nghia on 11/29/14.
+//  Created by LeVan Nghia on 11/14/14.
 //  Copyright (c) 2014 Le Van Nghia. All rights reserved.
 //
 
 import UIKit
+import QuartzCore
 
-open class MKLabel: UILabel {
+@IBDesignable
+open class MKSegmentControl : UIButton {
+    
+    /*public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayer()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupLayer()
+    }*/
+    @IBInspectable open var padding: CGSize = CGSize(width: 5, height: 5)
+    
     @IBInspectable open var maskEnabled: Bool = true {
         didSet {
             mkLayer.maskEnabled = maskEnabled
@@ -25,7 +39,7 @@ open class MKLabel: UILabel {
             mkLayer.elevation = elevation
         }
     }
-    @IBInspectable override open var shadowOffset: CGSize {
+    @IBInspectable open var shadowOffset: CGSize = CGSize.zero {
         didSet {
             mkLayer.shadowOffset = shadowOffset
         }
@@ -60,25 +74,62 @@ open class MKLabel: UILabel {
             mkLayer.backgroundAnimationEnabled = backgroundAnimationEnabled
         }
     }
-
+    
     override open var bounds: CGRect {
         didSet {
             mkLayer.superLayerDidResize()
         }
     }
-    fileprivate lazy var mkLayer: MKLayer = MKLayer(withView: self)
-
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupLayer()
+    
+    
+    @IBInspectable open var bottomBorderEnabled: Bool = true {
+        didSet {
+            bottomBorderLayer?.removeFromSuperlayer()
+            bottomBorderLayer = nil
+            if bottomBorderEnabled {
+                bottomBorderLayer = CALayer()
+                bottomBorderLayer?.frame = CGRect(x: 0, y: layer.bounds.height - 1, width: bounds.width, height: 1)
+                bottomBorderLayer?.backgroundColor = UIColor.MKColor.Grey.P500.cgColor
+                layer.addSublayer(bottomBorderLayer!)
+            }
+        }
     }
-
-    override public init(frame: CGRect) {
+    var screenWidth = UIScreen.main.bounds.size.width
+    @IBInspectable open var rightBorderEnabled: Bool = true {
+        didSet {
+            rightBorderLayer?.removeFromSuperlayer()
+            rightBorderLayer = nil
+            if rightBorderEnabled {
+                rightBorderLayer = CALayer()
+                rightBorderLayer?.frame = CGRect(x:screenWidth/2, y: 0, width: 1, height: bounds.height)
+                rightBorderLayer?.backgroundColor = UIColor.MKColor.Grey.P500.cgColor
+                layer.addSublayer(rightBorderLayer!)
+            }
+        }
+    }
+    @IBInspectable open var bottomBorderWidth: CGFloat = 1.0
+    @IBInspectable open var bottomBorderColor: UIColor = UIColor.lightGray {
+        didSet {
+            if bottomBorderEnabled {
+                bottomBorderLayer?.backgroundColor = bottomBorderColor.cgColor
+            }
+        }
+    }
+    @IBInspectable open var bottomBorderHighlightWidth: CGFloat = 1.75
+    
+    fileprivate lazy var mkLayer: MKLayer = MKLayer(withView: self)
+    fileprivate var floatingLabel: UILabel!
+    fileprivate var bottomBorderLayer: CALayer?
+    fileprivate var rightBorderLayer: CALayer?
+    /*override public init(frame: CGRect) {
         super.init(frame: frame)
         setupLayer()
     }
-
-    // MARK: Setup
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupLayer()
+    }*/
     fileprivate func setupLayer() {
         mkLayer.elevation = self.elevation
         self.layer.cornerRadius = self.cornerRadius
@@ -90,24 +141,38 @@ open class MKLabel: UILabel {
         mkLayer.rippleEnabled = self.rippleEnabled
         mkLayer.backgroundAnimationEnabled = self.backgroundAnimationEnabled
         mkLayer.setRippleColor(self.rippleLayerColor)
+        
+        layer.borderWidth = 1.0
+        
+        
+        addSubview(floatingLabel)
     }
-
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        bottomBorderLayer?.backgroundColor = isFirstResponder ? tintColor.cgColor : bottomBorderColor.cgColor
+        let borderWidth = isFirstResponder ? bottomBorderHighlightWidth : bottomBorderWidth
+        bottomBorderLayer?.frame = CGRect(x: 0, y: layer.bounds.height - borderWidth, width: layer.bounds.width, height: borderWidth)
+        
+        
+    }
     // MARK: Touch
     open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         mkLayer.touchesBegan(touches, withEvent: event)
     }
-
+    
     open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         mkLayer.touchesEnded(touches, withEvent: event)
     }
-
+    
     open override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
         mkLayer.touchesCancelled(touches, withEvent: event)
     }
-
+    
     open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         mkLayer.touchesMoved(touches, withEvent: event)
